ef53db0425323f11be7e7bd55d8965aadaff3aa6
devloliconic <kirillevsmirnov@gmail.com>
2022-07-01 17:32:47 +0700

    add sizeChange

---
+++ app/main.dae90f3c73c59a99ae8f.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";let o=document.querySelectorAll(".main__size");console.log(o);for(const e of o)e.addEventListener("click",(()=>{"transparent"==e.style.backgroundColor?e.style.backgroundColor="#B2B5BB":e.style.backgroundColor="transparent"}))})();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 import 'normalize.css';
 
 import '../scss/main.scss';
+
+
+
+let size = document.querySelectorAll(".main__size");
+console.log(size);
+
+for (const sizes of size) {
+    sizes.addEventListener("click", () => {
+        if(sizes.style.backgroundColor == "transparent"){
+            sizes.style.backgroundColor = "#B2B5BB";
+        }
+        else{
+            sizes.style.backgroundColor = "transparent";
+        }
+    });
+}
+
