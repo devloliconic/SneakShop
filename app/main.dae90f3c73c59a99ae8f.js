e5da21bb8c9edcc1f89837bed1984b5f2bc12d75
devloliconic <kirillevsmirnov@gmail.com>
2022-07-02 20:32:23 +0700

    add accordion menu

--- app/main.dae90f3c73c59a99ae8f.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";let o=document.querySelectorAll(".main__size");console.log(o);for(const e of o)e.addEventListener("click",(()=>{"transparent"==e.style.backgroundColor?e.style.backgroundColor="#B2B5BB":e.style.backgroundColor="transparent"}))})();
--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,35 @@ for (const sizes of size) {
     });
 }
 
+let q1 = document.getElementById("question1");
+q1.style.display = "none";
+let plus = document.getElementById("plus1");
+plus.onclick = function(){
+    if(q1.style.display == "none"){
+        q1.style.display = "block";
+        plus.className  = "question__btn question__btn_close";
+    }
+    else{
+        q1.style.display = "none";
+        plus.className  = "question__btn";
+    }
+    
+}
+let q2 = document.getElementById("question2");
+q2.style.display = "none";
+let plus2 = document.getElementById("plus2");
+plus2.onclick = function(){
+    if(q2.style.display == "none"){
+        q2.style.display = "block";
+        plus2.className  = "question__btn question__btn_close";
+    }
+    else{
+        q2.style.display = "none";
+        plus2.className  = "question__btn";
+    }
+    
+}
+
+// function showQ(){
+//     q1.style.display = "block";
+// }
